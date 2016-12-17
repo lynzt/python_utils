f0a8c80cf32df09716bafb1410871c75c6d836f5
lindsay <techells22@gmail.com>
2016-12-17 14:57:58 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 print find_packages()
 setup(name='python_utils',
-    version='0.0.1',
+    version='0.0.2',
     description='common functions i find myself writing over and over',
     author='lynzt',
     url='https://github.com/lynzt/python_utils',
